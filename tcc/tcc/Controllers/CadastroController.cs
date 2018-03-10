96655c60aca7197ff26f79185b0a8b7f178ca60a
Gabriel-Cordeiro <gabrielsilva.ipms@gmail.com>
2018-03-10 01:39:46 -0300

    First commit

---
+++ tcc/tcc/Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using tcc.Models;
+
+namespace tcc.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult CadastraUsuario(tb_usuarios teste)
+        {
+            ModelState.AddModelError("TxSenha", "Usuário Desativado!");
+
+            return View();
+        }
+    }
+}
